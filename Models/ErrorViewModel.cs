3b802e47f787fb1a2632b40ccd18cf5c99e0c479
Rafael <moreira.ramires@gmail.com>
2023-02-21 11:06:38 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoFinanceiro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
